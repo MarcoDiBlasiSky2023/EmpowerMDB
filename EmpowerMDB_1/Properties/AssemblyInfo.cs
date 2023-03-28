a723e3d886c7be14e2a9f9e169a5a1382761ce95
Marco Di Blasi <marco.diblasi.lutech@skytv.it>
2023-03-28 14:13:22 +0200

    Aggiungere i file di progetto.

---
+++ EmpowerMDB_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerMDB_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerMDB_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("79B20D55-9395-4474-AD5C-48EDAD8A0E0A")]
+[assembly: AssemblyVersion("1.0.0.0")]
