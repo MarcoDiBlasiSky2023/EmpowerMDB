3605d26aece4c47a374f6923712ec4357223c2d9
Marco Di Blasi <marco.diblasi.lutech@skytv.it>
2023-03-28 16:37:27 +0200

    Order Dispached

--- EmpowerMDB_1/EmpowerMDB_1.cs
+++ EmpowerMDB_1/EmpowerMDB_1.cs
@@ -53,6 +53,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using Empower.Library.Room3;
+
 using Skyline.DataMiner.Automation;
 using Skyline.DataMiner.Core.DataMinerSystem;
 using Skyline.DataMiner.Core.DataMinerSystem.Automation;
@@ -68,13 +70,16 @@ public class Script
 	/// <param name="engine">Link with SLAutomation process.</param>
 	public void Run(Engine engine)
 	{
-		engine.GenerateInformation("Hello world");
+		//engine.GenerateInformation("Hello world");
 
-		var myDms = engine.GetDms();
-		var elms = myDms.GetElements();
-		foreach (var elm in elms)
-		{
-			engine.GenerateInformation(elm.Name);
-		}
+		//var myDms = engine.GetDms();
+		//var elms = myDms.GetElements();
+		//foreach (var elm in elms)
+		//{
+		//	engine.GenerateInformation(elm.Name);
+		//}
+
+		var order = OrderFactory.CreateOrder("Order 1");
+		order.Dispatch("MDB");
 	}
 }
